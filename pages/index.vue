7fdd4f024bdcd2b3a27d9d1fac679b98c7c78c63
Hiroki Osame <hiroki@weebly.com>
2017-07-18 12:34:49 -0700

    Init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <test-component />
+  </section>
+</template>
+
+<script>
+import TestComponent from 'vue-ssr-repro-component'
+
+export default {
+  components: {
+    TestComponent
+  }
+}
+</script>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
